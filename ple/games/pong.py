83c8be084176eb7fa7be7ff839129a0cfb9a7154
AVELAUW <67564878+AVELAUW@users.noreply.github.com>
2021-01-19 21:39:17 -0800

    Update pong.py

--- ple/games/pong.py
+++ ple/games/pong.py
@@ -389,7 +389,7 @@ class Pong(PyGameWrapper):
         
         font = pygame.font.Font(None, self.ball_radius*4)
         text = font.render(str(self.score_counts['agent']),1,(255,255,255))
-        self.screen.blit(text, ((self.width*0.95/2)-(self.ball_radius*2),self.ball_radius*2) )
+        self.screen.blit(text, ((self.width*0.95/2)-(self.ball_radius*4),self.ball_radius*4) )
         text = font.render(str(self.score_counts['cpu']),1,(255,255,255))
         self.screen.blit(text, (self.width*1.05/2,self.ball_radius*4) )
         self.players_group.draw(self.screen)
